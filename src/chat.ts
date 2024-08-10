8834f54766dbd6f1452325a5b60040af360b8c66
paul6325106 <paul6325106@gmail.com>
2024-08-10 18:41:43 +1000

    chat

---
+++ src/chat.ts
@@ -0,0 +1,61 @@
+import ComfyJS, { OnCheerExtra, OnCheerFlags, OnMessageExtra, OnMessageFlags, OnSubExtra, SubMethods } from "comfy.js";
+
+type MessageCallback = (
+    user: string,
+    message: string,
+    flags: OnMessageFlags,
+    self: boolean,
+    extra: OnMessageExtra
+) => void;
+
+type CheerCallback = (
+    user: string,
+    message: string,
+    bits: number,
+    flags: OnCheerFlags,
+    extra: OnCheerExtra,
+) => void;
+
+type SubCallback = (
+    user: string,
+    message: string,
+    subTierInfo: SubMethods,
+    extra: OnSubExtra,
+) => void;
+
+export default function Chat(channelName: string) {
+    let chatCallback: MessageCallback | null = null;
+    const onChat = (callback: MessageCallback) => {
+        chatCallback = callback;
+    }
+
+    let cheerCallback: CheerCallback | null = null;
+    const onCheer = (callback: CheerCallback) => {
+        cheerCallback = callback;
+    }
+
+    let subCallback: SubCallback | null = null;
+    const onSub = (callback: SubCallback) => {
+        subCallback = callback;
+    }
+
+    ComfyJS.Init(channelName);
+
+    ComfyJS.onChat = (user, message, flags, self, extra) => {
+        chatCallback && chatCallback(user, message, flags, self, extra);
+    }
+
+    ComfyJS.onCheer = (user, message, bits, flags, extra) => {
+        cheerCallback && cheerCallback(user, message, bits, flags, extra);
+    }
+
+    ComfyJS.onSub = (user, message, subTierInfo, extra) => {
+        subCallback && subCallback(user, message, subTierInfo, extra);
+    }
+
+    return {
+        onChat,
+        onCheer,
+        onSub,
+    }
+}
